caad57e1ff9b2e4dd17bab65692e5a92db0ebb0a
Matjaž Bravc <matjaz.bravc@comtradegaming.com>
2024-09-27 16:04:29 +0200

    Solution ported to .NET 8

--- src/Services.Gateway/Program.cs
+++ src/Services.Gateway/Program.cs
@@ -1,40 +1,46 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Ocelot.Provider.Polly;
 using System.IO;
 
-namespace Services.Gateway
-{
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			CreateHostBuilder(args).Build().Run();
-		}
+IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args)
+  .UseContentRoot(Directory.GetCurrentDirectory())
+  .ConfigureWebHostDefaults(webBuilder =>
+  {
+    webBuilder.ConfigureServices(services =>
+      services.AddOcelot()
+        .AddConsul()
+        .AddCacheManager(x =>
+        {
+          x.WithDictionaryHandle();
+        })
+        .AddPolly());
+    webBuilder.Configure(app =>
+      app.UseOcelot().Wait())
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+      config
+        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+        .AddJsonFile("appsettings.json", false, true)
+        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+        .AddJsonFile("ocelot.json", false, false)
+        .AddEnvironmentVariables();
+    })
+    .ConfigureLogging((builderContext, logging) =>
+    {
+      logging.AddConfiguration(builderContext.Configuration.GetSection("Logging"));
+      logging.AddConsole();
+      logging.AddDebug();
+      logging.AddEventSourceLogger();
+    });
+  });
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.UseContentRoot(Directory.GetCurrentDirectory())
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				})
-				.ConfigureAppConfiguration((hostingContext, config) =>
-				{
-					config
-						.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-						.AddJsonFile("appsettings.json", false, true)
-						.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
-						.AddJsonFile("ocelot.json", false, false)
-						.AddEnvironmentVariables();
-				})
-				.ConfigureLogging((builderContext, logging) =>
-				{
-					logging.AddConfiguration(builderContext.Configuration.GetSection("Logging"));
-					logging.AddConsole();
-					logging.AddDebug();
-					logging.AddEventSourceLogger();
-				});
-	}
-}
+IHost host = hostBuilder.Build();
+await host.RunAsync();
