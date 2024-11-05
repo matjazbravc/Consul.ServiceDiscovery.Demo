e079d5792cc32cc4182bbd42bad8339afb2a2cfe
Matjaž Bravc <matjaz.bravc@comtradegaming.com>
2024-11-05 13:57:51 +0100

    Updated NuGet packages

--- src/Services.Gateway/Services/MyConsulServiceBuilder.cs
+++ src/Services.Gateway/Services/MyConsulServiceBuilder.cs
@@ -1,13 +1,14 @@
 ﻿using Consul;
+using Microsoft.AspNetCore.Http;
 using Ocelot.Logging;
 using Ocelot.Provider.Consul;
 using Ocelot.Provider.Consul.Interfaces;
-using System;
 
 namespace Services.Gateway.Services;
 
 // MORE INFO: https://ocelot.readthedocs.io/en/latest/features/servicediscovery.html#consul-service-builder
-public class MyConsulServiceBuilder(Func<ConsulRegistryConfiguration> configurationFactory, IConsulClientFactory clientFactory, IOcelotLoggerFactory loggerFactory) : DefaultConsulServiceBuilder(configurationFactory, clientFactory, loggerFactory)
+public class MyConsulServiceBuilder(IHttpContextAccessor contextAccessor, IConsulClientFactory clientFactory, IOcelotLoggerFactory loggerFactory)
+  : DefaultConsulServiceBuilder(contextAccessor, clientFactory, loggerFactory)
 {
   // I want to use the agent service IP address as the downstream hostname
   protected override string GetDownstreamHost(ServiceEntry entry, Node node) => entry.Service.Address;
